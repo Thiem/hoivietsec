c07dc70af68b4abdbdc4462af4d607cb55b681c1
Tran Thiem <tranthiem1993@gmail.com>
2014-07-01 23:10:52 +0700

    header trong folder include

--- Include/Header.php
+++ Include/Header.php
@@ -11,6 +11,15 @@ include_once("Class/ClassTableNotice.php");
 include_once("Class/ClassTableAdvert.php");
 include_once("Class/ClassTableLink.php");
 include_once("Class/ClassTableView.php");
+include_once("Class/ScheduleClass.php");
+include_once("Class/IntroductionClass.php");
+include_once("Class/PhotoGalleryClass.php");
+include_once("Class/PhotoCategoryClass.php");
+include_once("Class/GroupListClass.php");
+include_once("Class/JobClass.php");
+include_once("Class/FaqsClass.php");
+include_once("Class/MemberClass.php");
+include_once("Class/ConnectionClass.php");
 $use1 = new Class_ConnectDatabase();?>
 <link rel="stylesheet" href="Css/style.css">
 <div class="banner">
@@ -18,22 +27,22 @@ $use1 = new Class_ConnectDatabase();?>
 <div class="main_menu">
     <ul class="nav">
         <li><a href="index.php">Trang chủ</a></li>
-        <li><a href="#">Giới thiệu</a>
+        <li><a href="Introduction.php">Giới thiệu</a>
             <ul id="subnav_news" class="subnav">
                 <li>
-                    <a href="#">Giới thiệu chung</a></li>
+                    <a href="Introduction.php">Giới thiệu chung</a></li>
                 <li>
-                    <a href="#">Điều lệ hội</a></li>
+                    <a href="Schedule.php">Điều lệ hội</a></li>
                 <li>
-                    <a href="#">Danh sách ban chấp hành</a></li>
+                    <a href="Association.php">Danh sách ban chấp hành</a></li>
 
             </ul>
         </li>
 
-        <li><a href="#">Hội viên</a>
+        <li><a href="Member.php">Hội viên</a>
             <ul id="subnav_news" class="subnav">
-                <li><a href="#">Đăng ký hội viên</a></li>
-                <li><a href="#">Danh sách hội viên</a></li>
+                <li><a href="MemberAdd.php">Đăng ký hội viên</a></li>
+                <li><a href="Member.php">Danh sách hội viên</a></li>
             </ul>
         </li>
         <li><a href="NewsPage.php">Tin tức</a>
@@ -48,26 +57,23 @@ $use1 = new Class_ConnectDatabase();?>
                 <?php } ?>
             </ul>
         </li>
-        <li><a href="#">Thư viện ảnh</a></li>
+        <li><a href="PhotoGallery.php">Thư viện ảnh</a></li>
         <li><a href="#" target="_blank">Diễn đàn</a></li>
-        <li><a href="#">Hỏi đáp</a>
+        <li><a href="Faqs.php">Hỏi đáp</a>
             <ul id="subnav_news" class="subnav">
-                <li><a href="#">Gửi câu hỏi</a></li>
-                <li><a href="#">Danh sách các câu hỏi đã trả lời</a></li>
+                <li><a href="Question.php">Gửi câu hỏi</a></li>
+                <li><a href="Faqs.php">Danh sách các câu hỏi đã trả lời</a></li>
             </ul>
         </li>
-        <li><a href="#">Liên hệ</a></li>
+        <li><a href="Contact.php">Liên hệ</a></li>
         <li>
         </li>
         <li><a href="#">Sơ đồ trang</a></li>
-        <li>
-            <a href="#">Cesky</a>
-        </li>
-        <li>
+        <li class="search">
             <form accept-charset="utf-8" action="" method="post" id="NewSearchForm" controller="news">
                 <div style="display:none;"><input type="hidden" value="POST" name="_method"></div>
                 <div class="input text"><input type="text" id="NewTitle" name="data[New][title]"></div>
-                <div class="submit"><input type="submit" value="Tìm"></div>
+                <div class="submit"><input id="Sub" type="submit" value="Tìm"></div>
             </form>
         </li>
 
